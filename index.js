9ef646b9b68371909554b5ae0c6beda84c4af18a
Notron <amirnotron@outlook.com>
2026-06-21 22:06:21 +0330

    Add bot setup without secrets

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const discord = require('discord.js');
+const { token } = require('./config.json');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
+    console.log("Strawberry Management is online!");
+});
+
+client.login(token);
